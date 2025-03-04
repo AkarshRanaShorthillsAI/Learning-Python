ca2e2d923fa7d27e42d88dd951038beacc41c291
AkarshRanaShorthillsAI <akarsh.rana@shorthills.ai>
2025-03-04 06:19:23 +0000

    Learn about variables in Python

--- Basics/variables.py
+++ Basics/variables.py
@@ -3,3 +3,40 @@ age = 22;
 
 print(name);
 print(age);
+
+# Basic Assignment
+x = 5
+y = 3.14
+z = "Hi"
+
+
+# Dynamic Typing
+x = 10
+x = "Now a string"
+
+# Multiple Assignments
+a = b = c = 100
+print(a, b, c) 
+
+#Assigning Different Values
+x, y, z = 1, 2.5, "Python"
+print(x, y, z) 
+
+# Getting the Type of Variable 
+# Define variables with different data types
+n = 42
+f = 3.14
+s = "Hello, World!"
+li = [1, 2, 3]
+d = {'key': 'value'}
+bool = True
+
+# Get and print the type of each variable
+print(type(n))   
+print(type(f)) 
+print(type(s))   
+print(type(li))     
+print(type(d))     
+print(type(bool))  
+
+
